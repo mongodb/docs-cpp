85a16f7a22effea9c6aefa4f2477d2839cd03547
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2024-06-25 15:37:43 -0500

    js feedback

---
+++ source/includes/assumes-instance.rst
@@ -0,0 +1,4 @@
+.. note:: mongocxx::instance
+
+   The code examples on this page assume that you've already created a ``mongocxx::instance``
+   object elsewhere in your application.
--- source/includes/connect.cpp
+++ source/includes/connect.cpp
@@ -5,11 +5,9 @@ mongocxx::instance instance{};
 // end-instance
 
 // start-client
-#include <mongocxx/instance.hpp>
 #include <mongocxx/uri.hpp>
 #include <mongocxx/client.hpp>
 
-mongocxx::instance instance{};
 mongocxx::uri uri("mongodb://localhost:27017");
 mongocxx::client client(uri);
 // end-client
