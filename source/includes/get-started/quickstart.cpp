a98dbe561100853caade46ebb9437bff9d4efe1d
norareidy <nora.reidy@mongodb.com>
2024-06-26 11:59:42 -0400

    KA feedback

--- source/includes/get-started/quickstart.cpp
+++ source/includes/get-started/quickstart.cpp
@@ -6,7 +6,6 @@
 #include <bsoncxx/json.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
-#include <mongocxx/stdx.hpp>
 #include <mongocxx/uri.hpp>
 
 using bsoncxx::builder::basic::kvp;
