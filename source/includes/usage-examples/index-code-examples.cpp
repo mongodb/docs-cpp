92b758d4be05abc1903e38e8ed6c8acadd3158dc
Jordan Smith <jordan.smith@mongodb.com>
2024-07-12 07:27:44 -0700

    add methods

--- source/includes/usage-examples/index-code-examples.cpp
+++ source/includes/usage-examples/index-code-examples.cpp
@@ -4,3 +4,15 @@ std::string result = collection.create_index(std::move(index_specification));
 
 std::cout << "Index created: " << result << std::endl;
 // end-single-field
+
+// start-remove-index
+collection.indexes().drop_one("title_1");
+
+std::cout << "Index 'title_1' dropped." << std::endl;
+// end-remove-index
+
+// start-remove-all-indexes
+collection.indexes().drop_all();
+
+std::cout << "All indexes removed." << std::endl;
+// end-remove-all-indexes
