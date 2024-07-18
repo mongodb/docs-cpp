c2b384dd1fe1c09eec1f5c16571601973e5ff5c3
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2024-07-18 11:59:07 -0700

    DOCSP-40810 Secure Data landing page (#25)

--- source/includes/authentication.cpp
+++ source/includes/authentication.cpp
@@ -1,72 +1,48 @@
 // start-scram-sha-256
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<username>:<password>@<hostname>:<port>/?"
                          "authSource=admin&authMechanism=SCRAM-SHA-256");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-scram-sha-256
 
 // start-scram-sha-1
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<username>:<password>@<hostname>:<port>/?"
                          "authSource=admin&authMechanism=SCRAM-SHA-1");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-scram-sha-1
 
 // start-x509
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<hostname>:<port>/?"
                          "tls=true&tlsCertificateKeyFile=path/to/client.pem&authMechanism=MONGODB-X509");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-x509
 
 // start-aws-connection-uri
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<AWS IAM access key ID>:<AWS IAM secret access key>@<hostname>:<port>/?"
                          "authMechanism=MONGODB-AWS");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-aws-connection-uri
 
 // start-aws-connection-uri-session
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<AWS IAM access key ID>:<AWS IAM secret access key>@<hostname>:<port>/?"
                          "authMechanism=MONGODB-AWSS&authMechanismProperties=AWS_SESSION_TOKEN:<token>");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-aws-connection-uri-session
 
 // start-aws-environment
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<hostname>:<port>/?"
                          "authMechanism=MONGODB-AWS");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-aws-environment
 
 // start-kerberos
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://mongodbuser%40EXAMPLE.COM@<hostname>:<port>/?"
                          "authMechanism=GSSAPI"
                          "&authMechanismProperties=SERVICE_NAME:<authentication service name>");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-kerberos
 
 // start-plain
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-
 auto uri = mongocxx::uri("mongodb://<username>:<password>@<hostname>:<port>/?"
                          "authMechanism=PLAIN&tls=true");
-mongocxx::client client(uri);
+auto client = mongocxx::client(uri);
 // end-plain
