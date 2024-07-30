4510d4366d909fc2266e2c847934c9bad3fa16f7
norareidy <nora.reidy@mongodb.com>
2024-07-30 12:31:07 -0400

    DOCSP-40784: Connect landing page

---
+++ source/includes/usage-examples/connect-code-examples.cpp
@@ -0,0 +1,90 @@
+#include <mongocxx/instance.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/exception/exception.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
+int main()
+{
+    mongocxx::instance instance;
+
+    {
+        // Retrieves one document that matches a query filter
+        // start-local
+        mongocxx::uri uri("mongodb://localhost:27017/");
+        mongocxx::client client(uri);
+        // end-local
+    }
+
+    {
+        // start-atlas
+        mongocxx::uri uri("<Atlas connection string>");
+        mongocxx::client client(uri);
+        // end-atlas
+    }
+
+    {
+        // Retrieves one document that matches a query filter
+        // start-replica-set
+        mongocxx::uri uri("mongodb://<replica set member>:<port>/?replicaSet=<replica set name>");
+        mongocxx::client client(uri);
+        // end-replica-set
+    }
+
+    {
+        // start-enable-tls
+        mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true");
+        mongocxx::client client(uri);
+        // end-enable-tls
+    }
+
+    {
+        // start-disable-ocsp
+        mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true&tlsDisableOCSPEndpointCheck=true");
+        mongocxx::client client(uri);
+        // end-disable-ocsp
+    }
+
+    {
+        // start-insecure-tls
+        mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true&tlsInsecure=true");
+        mongocxx::client client(uri);
+        // end-insecure-tls
+    }
+
+    {
+        // start-disable-hostname
+        mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true&tlsAllowInvalidHostnames=true");
+        mongocxx::client client(uri);
+        // end-disable-hostname
+    }
+
+    {
+        // start-compression-all
+        mongocxx::uri uri("mongodb://<hostname>:<port>/?compressors=snappy,zstd,zlib");
+        mongocxx::client client(uri);
+        // end-compression-all
+    }
+
+    {
+        // start-compression-zlib
+        mongocxx::uri uri("mongodb://<hostname>:<port>/?compressors=zlib&zlibCompressionLevel=1");
+        mongocxx::client client(uri);
+        // end-compression-zlib
+    }
+
+    {
+        // Retrieves one document that matches a query filter
+        // start-stable-api
+        mongocxx::uri uri("<connection string>");
+        
+        mongocxx::options::client client_options;
+        mongocxx::options::server_api server_api_options(mongocxx::options::server_api::version::k_version_1);
+        client_options.server_api_opts(server_api_options);
+        mongocxx::client client(uri, client_options);
+        // end-stable-api
+    }
+}
