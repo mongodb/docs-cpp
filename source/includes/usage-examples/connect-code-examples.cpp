c6c05681400cfd00c04dc16c393a46a8b5b56e84
norareidy <nora.reidy@mongodb.com>
2024-07-31 13:56:34 -0400

    JS feedback

--- source/includes/usage-examples/connect-code-examples.cpp
+++ source/includes/usage-examples/connect-code-examples.cpp
@@ -12,7 +12,7 @@ int main()
     mongocxx::instance instance;
 
     {
-        // Retrieves one document that matches a query filter
+        // Connects to a local MongoDB deployment
         // start-local
         mongocxx::uri uri("mongodb://localhost:27017/");
         mongocxx::client client(uri);
@@ -20,6 +20,7 @@ int main()
     }
 
     {
+        // Connects to a MongoDB Atlas deployment
         // start-atlas
         mongocxx::uri uri("<Atlas connection string>");
         mongocxx::client client(uri);
@@ -27,7 +28,7 @@ int main()
     }
 
     {
-        // Retrieves one document that matches a query filter
+        // Connects to a replica set
         // start-replica-set
         mongocxx::uri uri("mongodb://<replica set member>:<port>/?replicaSet=<replica set name>");
         mongocxx::client client(uri);
@@ -35,6 +36,7 @@ int main()
     }
 
     {
+        // Connects to a MongoDB deployment and enables TLS
         // start-enable-tls
         mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true");
         mongocxx::client client(uri);
@@ -42,6 +44,7 @@ int main()
     }
 
     {
+        // Connects to a MongoDB deployment, enables TLS, and prevents OCSP endpoint checks
         // start-disable-ocsp
         mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true&tlsDisableOCSPEndpointCheck=true");
         mongocxx::client client(uri);
@@ -49,11 +52,13 @@ int main()
     }
 
     {
+        // Connects to a TLS-enabled deployment and instructs the driver to check the
+        // server certificate against a CRL
         // start-crl
         mongocxx::options::client client_options;
         mongocxx::options::tls tls_options;
 
-        tls_options.crl_file("/path/to/file.pem");
+        tls_options.crl_file("<path to your CRL file>");
         client_options.tls_opts(tls_options);
 
         mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true");
@@ -62,6 +67,7 @@ int main()
     }
 
     {
+        // Connects to a TLS-enabled deployment and disables server certificate verification
         // start-insecure-tls
         mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true&tlsInsecure=true");
         mongocxx::client client(uri);
@@ -69,6 +75,7 @@ int main()
     }
 
     {
+        // Connects to a TLS-enabled deployment and disables hostname verification
         // start-disable-hostname
         mongocxx::uri uri("mongodb://<hostname>:<port>/?tls=true&tlsAllowInvalidHostnames=true");
         mongocxx::client client(uri);
@@ -83,6 +90,7 @@ int main()
     }
 
     {
+        // Enables compression for a MongoDB connection and specifies each compression algorithm
         // start-compression-zlib
         mongocxx::uri uri("mongodb://<hostname>:<port>/?compressors=zlib&zlibCompressionLevel=1");
         mongocxx::client client(uri);
@@ -90,7 +98,7 @@ int main()
     }
 
     {
-        // Retrieves one document that matches a query filter
+        // Connects to a MongoDB deployment and enables the stable API
         // start-stable-api
         mongocxx::uri uri("<connection string>");
         
