bb3d7319fe9fcf02c91dcf1ff16bdcfb42521371
your proper name <your github email>
2024-10-22 16:07:12 -0400

    (DOCSP-40807) Fixing toctree error and deleting copies of example code.

--- source/includes/indexes/indexes.cpp
+++ source/includes/indexes/indexes.cpp
@@ -54,24 +54,3 @@ int main(){
     }
 
 }
-
-// start-db-coll
-auto db = client["sample_mflix"];
-auto collection = db["movies"];
-// end-db-coll
-// start-index-single
-auto index_specification = make_document(kvp("title", 1));
-collection.create_index(index_specification.view());
-// end-index-single
-
-// start-remove-index
-collection.indexes().drop_one("title_1");
-// end-remove-index
-
-// start-remove-all-indexes
-collection.indexes().drop_all();
-// end-remove-all-indexes
-
-// start-remove-all-wildcard
-collection.indexes().drop_one("*");
-// end-remove-all-wildcard
