1ec552d6ce84f9fbd9785941867a2cdde9c3a8f1
your proper name <your github email>
2024-10-22 15:48:59 -0400

    (DOCSP-40807) Add compound indexes to work-with-indexes page and code examples page.

--- source/includes/indexes/indexes.cpp
+++ source/includes/indexes/indexes.cpp
@@ -1,3 +1,64 @@
+#include <iostream>
+
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
+
+int main(){
+
+    mongocxx::instance instance;
+    mongocxx::uri uri("<connection string>");
+    mongocxx::client client(uri);
+
+    // start-db-coll
+    auto db = client["sample_mflix"];
+    auto collection = db["movies"];
+    // end-db-coll
+
+    {
+        // start-index-single
+        auto index_specification = make_document(kvp("title", 1));
+        collection.create_index(index_specification.view());
+        // end-index-single
+    }
+
+    {
+        // start-index-compound
+        auto index_specification = make_document(kvp("title", 1), kvp("year", -1));
+        collection.create_index(index_specification.view());
+        // end-index-compound
+    }
+
+    {
+        // start-remove-index
+        collection.indexes().drop_one("title_1");
+        // end-remove-index
+    }
+
+    {
+        // start-remove-all-indexes
+        collection.indexes().drop_all();
+        // end-remove-all-indexes
+    }
+    
+    {
+        // start-remove-all-wildcard
+        collection.indexes().drop_one("*");
+        // end-remove-all-wildcard    
+    }
+
+}
+
+// start-db-coll
+auto db = client["sample_mflix"];
+auto collection = db["movies"];
+// end-db-coll
 // start-index-single
 auto index_specification = make_document(kvp("title", 1));
 collection.create_index(index_specification.view());
