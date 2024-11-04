91ad61dab663f0752fbd7a60c6803e52e384c47f
Elyse Foreman <elyse.foreman@mongodb.com>
2024-11-04 17:39:13 -0500

    (DOCSP-45011) Edits.

---
+++ source/includes/run-command.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
+int main() {
+    mongocxx::instance instance;
+    mongocxx::uri uri("<connection string>");
+    mongocxx::client client(uri);
+  
+    // start-db-coll
+    auto db = client["sample_mflix"];
+    auto collection = db["movies"];
+    // end-db-coll
+
+    {
+        // start-run-hello
+        // Build the "hello" command 
+        auto command = make_document(kvp("hello",1));
+        auto result = db.run_command(command.view());       
+
+        std::cout << bsoncxx::to_json(result) << std::endl; 
+        // end-run-hello 
+
+    }
+
+}
