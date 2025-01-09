e64dba61f1deb1dcad57bcd562ee80adbe784b5f
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2025-01-09 13:22:18 -0500

    clean up sample code

--- source/includes/authentication.cpp
+++ source/includes/authentication.cpp
@@ -49,10 +49,11 @@ auto client = mongocxx::client(uri);
 
 // start-auth-err
 try {
-    auto uri = mongocxx::uri("mongodb://<db_username>:<db_password>@<hostname>:<port>/?"
-                         "authSource=admin&authMechanism=SCRAM-SHA-256");
+    auto uri = mongocxx::uri("<Your chosen auth method>");
     auto client = mongocxx::client(uri);
-    client["db"].run_command(bsoncxx::from_json(R"({"ping": 1})")); // Any command requiring authorization
+
+    client["db"].run_command("<Any command requiring authorization>");
+    
 } catch (const mongocxx::exception& ex) {
     std::cerr << "Error: " << ex.what() << std::endl;
 }
